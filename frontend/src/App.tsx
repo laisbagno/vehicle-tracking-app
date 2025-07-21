bb9a8ac21fa02a41ec0d520fc73d3c5603fea034
laisbagno <laisbags@gmail.com>
2025-07-21 12:52:36 -0300

    Feat: adicionando favicon e logo do projeto + tradução do tittle html

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
 import 'leaflet/dist/leaflet.css';
 import './styles/global.scss';
 import { AppRoutes } from './routes/AppRoutes';
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    document.title = t('app.title');
+  }, [t]);
   return <AppRoutes />;
 }
 
--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,13 +1,17 @@
 import { useTranslation } from 'react-i18next';
 import LanguageSwitcher from '../LanguageSwitcher/LanguageSwitcher';
 import styles from './Header.module.scss';
+import Logo from '../../assets/logo.jpeg';
 
 const Header = () => {
   const { t } = useTranslation();
 
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>{t('app.title')}</h1>
+      <h1 className={styles.title}>
+        <img className={styles.img} src={Logo} />
+        {t('app.title')}
+      </h1>
       <LanguageSwitcher />
     </header>
   );
